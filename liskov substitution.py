212d565faec3fbd03c2c30ce91f152dfbccad342
Константин <MindlinKS@yandex.ru>
2023-02-28 01:29:34 +0300

    add liskov substitution.py

--- liskov substitution.py
+++ liskov substitution.py
@@ -2,7 +2,7 @@
  principle, демонстрирующий возможность подстановки дочернего класса"""
 
 
-class Printer():
+class Printer:
     def __init__(self, name):
         self.name = name
 
@@ -23,4 +23,3 @@ black_1128.print_text()
 color_3512 = PhotoPrinter('color_3512')
 color_3512.print_text()  # Можно заменить родительский класс на дочерний.
 color_3512.print_photo()
-
