07e91185f8533f140ec91e7ed45787788ccd4cef
MindlinKS@yandex.ru <MindlinKS@yandex.ru>
2023-02-28 00:44:08 +0300

    add some_decorator

--- decorators.py
+++ decorators.py
@@ -1,22 +1,43 @@
+"""Буду складывать сюда декораторы для дальнейшего использования"""
 import time
 
+# Основная идея декоратора в добавлении дополнительного поведения функции или
+# классу. Пример ниже.
 
-def average_function_speed_decorator(func):
-    """Декоратор печатающий среднее время выполнения функции"""
+
+def some_decorator(func):
     def wrapper(*args, **kwargs):
-        start = time.time()
-        for _ in range(1000):
-            func(*args, **kwargs)
-        end = time.time()
-        print((end - start) / 1000)
+        print('Поведение до выполнения функции')
+        func(*args, **kwargs)
+        print('Поведение после выполнения функции')
     return wrapper
 
 
-@average_function_speed_decorator
-def last_sequence_number(number: int):
-    for cur_number in range(number + 1):
-        if cur_number == number:
-            return cur_number
+@some_decorator
+def print_hi():
+    print('Hi')
 
 
-last_sequence_number(1000)
+
+print_hi()
+
+
+# def average_function_speed_decorator(func):
+#     """Декоратор печатающий среднее время выполнения функции"""
+#     def wrapper(*args, **kwargs):
+#         start = time.time()
+#         for _ in range(1000):
+#             func(*args, **kwargs)
+#         end = time.time()
+#         print((end - start) / 1000)
+#     return wrapper
+#
+#
+# @average_function_speed_decorator
+# def last_sequence_number(number: int):
+#     for cur_number in range(number + 1):
+#         if cur_number == number:
+#             return cur_number
+#
+#
+# last_sequence_number(1000)
