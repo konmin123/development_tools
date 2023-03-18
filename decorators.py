c61990723cffc74c6d56e14c23ab1218e5ebb898
Константин <MindlinKS@yandex.ru>
2023-03-18 22:46:59 +0300

    add decorator with argument

--- decorators.py
+++ decorators.py
@@ -45,23 +45,3 @@ def sleep_time(t: int):
 
 
 sleep_time(5)
-
-# def average_function_speed_decorator(func):
-#     """Декоратор печатающий среднее время выполнения функции"""
-#     def wrapper(*args, **kwargs):
-#         start = time.time()
-#         for _ in range(1000):
-#             func(*args, **kwargs)
-#         end = time.time()
-#         print((end - start) / 1000)
-#     return wrapper
-#
-#
-# @average_function_speed_decorator
-# def last_sequence_number(number: int):
-#     for cur_number in range(number + 1):
-#         if cur_number == number:
-#             return cur_number
-#
-#
-# last_sequence_number(1000)
