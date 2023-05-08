d3bcb428d2a5033ad9f5b63d0da9116d7487fe31
MindlinKS@yandex.ru <MindlinKS@yandex.ru>
2023-05-08 13:09:28 +0300

    thread_mixin.py

--- mixins/thread_mixin.py
+++ mixins/thread_mixin.py
@@ -1,39 +1,6 @@
-"""class Parser:
-    def send_request(self):
-        print("Отправляю запрос...")
-        time.sleep(1)
-
-    def pars_request(self):
-        print("Парсю результат...")
-        time.sleep(0.75)
-
-avito = Parser()
-avito.send_request()
-avito.pars_request()
-
-Выполняется две секунды
-
-
-Пример с миксином:
-class Parser(ThreadMixin):
-    def send_request(self):
-        print("Отправляю запрос...")
-        time.sleep(1)
-
-    def pars_request(self):
-        print("Парсю результат...")
-        time.sleep(0.75)
-
-avito = Parser()
-avito.send_request()
-avito.pars_request()
-
-Выполняется 1 секунду
-(Тк ThreadMixin добавляет всем методам класса многопоточность)"""
+"""Миксин позволяющий методам работать класса в многопоточном режиме."""
 import threading
-import time
-
-from time import sleep
+from time import time, sleep
 
 
 class ThreadMixin:
@@ -68,8 +35,8 @@ class Parser(ThreadMixin):
 
 
 avito = Parser()
-start = time.time()
+start = time()
 avito.send_request()
 avito.pars_request()
-end = time.time()
+end = time()
 print(end - start)
